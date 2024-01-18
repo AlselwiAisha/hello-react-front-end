c28e2d22965fd662343a529b1b25aa9e30956c54
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2024-01-18 05:56:45 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
 import Greeting from './Components/Greeting';
+import store from './redux/store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Routes>
-          <Route path="/" exact component={Greeting} />
-        </Routes>
-      </header>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <Routes>
+              <Route path="/" exact component={Greeting} />
+            </Routes>
+          </header>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
